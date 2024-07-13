cabb6e12ccfcb53e3b88c0f975814b25884fe209
tolluset <dlqud19@gmail.com>
2024-07-13 21:07:15 +0900

    chore: add description

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,16 +1,22 @@
 import { signIn } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 export default function SignIn() {
   return (
     <main className="flex h-full  flex-col items-center justify-between p-4">
-      <div className="flex items-center h-full">
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-2xl font-medium">Just memo it.</h1>
+        <p className="text-sm text-muted-foreground">Sign in to continue</p>
+
+        <br />
+
         <form
           action={async () => {
             "use server";
             await signIn("google", { redirectTo: "/" });
           }}
         >
-          <button type="submit">Signin with Google</button>
+          <Button type="submit">Signin with Google</Button>
         </form>
       </div>
     </main>
